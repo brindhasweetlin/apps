89100ca764932d51df40c87f976333f73785b7fb
brindhasweetlin <109205575+brindhasweetlin@users.noreply.github.com>
2025-09-16 19:11:48 +0530

    app theme

--- safe_bet_app/lib/theme/app_theme.dart
+++ safe_bet_app/lib/theme/app_theme.dart
@@ -30,7 +30,7 @@ class AppTheme {
           ),
         ),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
@@ -67,7 +67,7 @@ class AppTheme {
           ),
         ),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 4,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
